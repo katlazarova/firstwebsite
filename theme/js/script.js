777ea37fbdf6d0f0704a7b4a93fd9ca974d7d7fa
katlazarova <katarinaalazarova@gmail.com>
2019-07-26 15:11:30 +0100

    greyscale and theme directory

---
+++ theme/js/script.js
@@ -0,0 +1,8 @@
+var swiper = new Swiper('.swiper-container', {
+      slidesPerView: 'auto',
+      spaceBetween: 30,
+      pagination: {
+        el: '.swiper-pagination',
+        clickable: true,
+      },
+    });
