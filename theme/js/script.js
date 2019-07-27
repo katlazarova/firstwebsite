3f85164f35ec65f81517cfce2f71fc1d788cf0fd
katlazarova <katarinaalazarova@gmail.com>
2019-07-27 21:25:44 +0100

    testimonial styling and js

--- theme/js/script.js
+++ theme/js/script.js
@@ -1,11 +1,16 @@
-$(document).ready(function () {
+$(document).ready(function (){
     //initialize swiper when document ready
-    var mySwiper = new Swiper ('.testimonial-container', {
+    var testimonialSwiper = new Swiper ('.swiper-container.testimonial-container', {
       // Optional parameters
       slidesPerView: 'auto',
       spaceBetween: 30,
       pagination: {
-        el: '.swiper-pagination',
-        clickable: true,
+      	el: '.swiper-pagination',
+      	clickable: true,
       },
-  });
+      navigation: {
+      	nextEl: '.swiper-button-next',
+      	prevEl: '.swiper-button-prev',
+      },
+    }),
+});
