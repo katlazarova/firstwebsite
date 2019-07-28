1f232315153da51f259fafb2d720b6c060d5640c
katlazarova <katarinaalazarova@gmail.com>
2019-07-28 22:43:09 +0100

    testimonial slides

--- theme/js/script.js
+++ theme/js/script.js
@@ -2,15 +2,11 @@ $(document).ready(function (){
     //initialize swiper when document ready
     var testimonialSwiper = new Swiper ('.swiper-container.testimonial-container', {
       // Optional parameters
-      slidesPerView: 'auto',
-      spaceBetween: 30,
+      slidesPerView: 3,
+      spaceBetween: 40,
       pagination: {
       	el: '.swiper-pagination',
-      	clickable: true,
+      	type: 'progressbar',
       },
-      navigation: {
-      	nextEl: '.swiper-button-next',
-      	prevEl: '.swiper-button-prev',
-      },
-    }),
+    })
 });
