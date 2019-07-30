7d7dc11e85215eb77bc50e00d0fb1adc039482f7
katlazarova <katarinaalazarova@gmail.com>
2019-07-30 23:59:02 +0100

    grunt watch and LEARNING

--- theme/Gruntfile.js
+++ theme/Gruntfile.js
@@ -10,13 +10,13 @@ module.exports = function(grunt) {
 				sourceMap: true,
 			},
 			dist: {
-				options: {
-					outputStyle: 'compressed'
-				},
-				files: {
-					'build/styles.min.css': 'sass/app.scss',
-				}
-			}
+		        options: {
+		          outputStyle: 'compressed'
+		        },
+		        files: {
+		          'build/styles.min.css': 'sass/app.scss'
+		        }
+     		}
 		},
 		concat:{
 			js: {
@@ -24,7 +24,7 @@ module.exports = function(grunt) {
 				src: ['js/**/*.js'],
 				// Compresses into one file and places into the build folder.
 				dest: 'build/scripts.js'
-			},
+			}
 		}, 
 		uglify: {
 			build: {
@@ -34,24 +34,35 @@ module.exports = function(grunt) {
 				}]
 			}
 		},
-
-		watch:{
-			scripts: {
-				files: ['js/**/*.js', 'sass/**/*.scss'],
-				tasks: 'default',
-				options: {
-					debounceDelay: 250,
-				},
-			},
-		},
+		watch: {
+	      sass: {
+	        files: ['sass/**/*.scss'],
+        	tasks: ['sass']
+	      }
+	    },
+		browserSync: {
+	      dev: {
+	        bsFiles: {
+	          src: [
+	            'build/*.css',
+	            'build/*.js',
+	            'img/*.*'
+	          ]
+	        },
+	        options: {
+	        	watchTask: true,
+	        	server: "../"
+	        }
+	      }
+	    },
 	});
 
 	grunt.loadNpmTasks('grunt-sass');
+	grunt.loadNpmTasks('grunt-browser-sync');
 	grunt.loadNpmTasks('grunt-contrib-concat');
 	grunt.loadNpmTasks('grunt-contrib-uglify');
 	grunt.loadNpmTasks('grunt-contrib-watch');
-	grunt.loadNpmTasks('grunt-browser-sync');
-
 	grunt.registerTask('default', ['sass', 'concat', 'uglify']);
+	grunt.registerTask('serve', ['default', 'browserSync', 'watch']);
 	//grunt.registerTask('watch', ['watch']);
 };
