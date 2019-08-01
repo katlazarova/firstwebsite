639cad8ebfa3fff09b0be5e825e0d3270fcfcc31
katlazarova <katarinaalazarova@gmail.com>
2019-08-01 10:26:12 +0100

    minor responsiveness changes

--- theme/build/scripts.js
+++ theme/build/scripts.js
@@ -1,4 +1,15 @@
+function myFunction() {
+  var x = document.getElementById("my-nav-links");
+  if (x.className === "nav-menu") {
+    x.className += " responsive";
+  } else {
+    x.className = "nav-menu";
+  }
+}
+
 $(document).ready(function (){
+
+  
     //initialize swiper when document ready
     var testimonialSwiper = new Swiper ('.swiper-container.testimonial-container', {
       // Optional parameters
