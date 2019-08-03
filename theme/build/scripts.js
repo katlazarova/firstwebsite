4b9892fac69adcd308811ad09d988af6c9f3a007
katlazarova <katarinaalazarova@gmail.com>
2019-08-03 18:02:52 +0100

    header styling changes and responsiveness of all homepage components

--- theme/build/scripts.js
+++ theme/build/scripts.js
@@ -2,8 +2,26 @@ $(document).ready(function() {
     //initialize swiper when document ready
     var testimonialSwiper = new Swiper('.swiper-container.testimonial-container', {
         // Optional parameters
-        slidesPerView: 3,
+        slidesPerView: 'auto',
         spaceBetween: 40,
+        breakpointsInverse: true,
+        breakpoints: {
+            576: {
+                slidesPerView: 2,
+                spaceBetween: 20
+            },
+
+            768: {
+                slidesPerView: 3,
+                spaceBetween: 30
+            },
+
+            1024: {
+                slidesPerView: 3,
+                spaceBetween: 40
+            },
+        },
+
         grabCursor: true,
         pagination: {
             el: '.swiper-pagination',
@@ -11,19 +29,19 @@ $(document).ready(function() {
         },
     });
 
-     $('nav ul li a:not(:only-child)').click(function(e) {
-      $(this).siblings('.nav-dropdown').toggle();
-      $('.dropdown').not($(this).siblings()).hide();
-      e.stopPropagation();
+    $('nav ul li a:not(:only-child)').click(function(e) {
+        $(this).siblings('.nav-dropdown').toggle();
+        $('.dropdown').not($(this).siblings()).hide();
+        e.stopPropagation();
     });
     $('html').click(function() {
-      $('.nav-dropdown').hide();
+        $('.nav-dropdown').hide();
     });
     $('#nav-toggle').click(function() {
-      $('nav ul').slideToggle();
+        $('nav ul').slideToggle();
     });
     $('#nav-toggle').on('click', function() {
-      this.classList.toggle('active');
+        this.classList.toggle('is-active');
     });
 });
 
