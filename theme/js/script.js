6e8f589e3c60f63bb6e8bdeb6ac61053578de1e1
katlazarova <katarinaalazarova@gmail.com>
2019-08-04 15:06:42 +0100

    bug fixes and js optimisation

--- theme/js/script.js
+++ theme/js/script.js
@@ -1,7 +1,7 @@
 $(document).ready(function() {
-    //initialize swiper when document ready
+    // Initialize swiper when document ready.
     var testimonialSwiper = new Swiper('.swiper-container.testimonial-container', {
-        // Optional parameters
+        // Optional parameters.
         slidesPerView: 'auto',
         spaceBetween: 40,
         breakpointsInverse: true,
@@ -21,35 +21,42 @@ $(document).ready(function() {
                 spaceBetween: 40
             },
         },
-
+        // Cursor changes when entering the swiper to alert the user that the slide is grabable.
         grabCursor: true,
         pagination: {
             el: '.swiper-pagination',
             type: 'progressbar',
         },
     });
+    // Calling the matchHeight function.
+    matchItemsHeights();
+    // Calling the menuFnctionality.
+    menuFunctionality();
 
+});
+
+// Renders different elements to assume the height of the tallest element - mathches their heights.
+function matchItemsHeights() {
+    $('.card-component-container .card-text-container').matchHeight();
+};
+
+function menuFunctionality() {
     $('nav ul li a:not(:only-child)').click(function(e) {
+        // If anchor is not an only child, it’s sibling must be a drop down list - when clicked, toggle hide and show.
         $(this).siblings('.nav-dropdown').toggle();
         $('.dropdown').not($(this).siblings()).hide();
         e.stopPropagation();
     });
     $('html').click(function() {
+        // When anywhere on the page is clicked, the dropdown menu hides.
         $('.nav-dropdown').hide();
     });
     $('#nav-toggle').click(function() {
+        // When clicking on the element, the nav ul shows or hides with a sliding motion.
         $('nav ul').slideToggle();
     });
     $('#nav-toggle').on('click', function() {
-        this.classList.toggle('is-active');
+        // Toggles 'is-active' class on click. 
+        $(this).toggleClass('is-active');
     });
-});
-
-function myFunction() {
-    var x = document.getElementById("nav-menu");
-    if (x.className === "header-links") {
-        x.className += "responsive";
-    } else {
-        x.className = "header-links";
-    }
-}
+};
