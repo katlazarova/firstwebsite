9e1055a54886fb9929523d351a570586360f0f72
katlazarova <katarinaalazarova@gmail.com>
2019-08-09 21:13:37 +0100

    about us hero image

--- about-us.php
+++ about-us.php
@@ -1,4 +1,9 @@
 <?php include 'includes/header.php';?>
-
+<div class="hero-image full-width bottom-spacer">
+	<div class="hero-text-box">
+		<h2>About Us</h2>
+		<p>Promoting equality for girls worlwide.</p>
+	</div> <!-- End hero-text-box -->
+</div> <!-- End hero image -->
 
 <?php include 'includes/footer.php';?>
