037f79fa50ad64f74d5607b51f639d750b15a07d
katlazarova <katarinaalazarova@gmail.com>
2019-09-10 09:21:42 +0100

    fix accordion styling and adding FAQ content

--- contact-us.php
+++ contact-us.php
@@ -1,19 +1,25 @@
 <?php include 'includes/header.php'; ?>
 <h1 class="bottom-spacer">Contact Us</h1>
-<div class="accordion-component">
-    <button class="accordion">Section 1</button>
+<h2>FAQ</h2>
+<div class="accordion-component bottom-spacer full-width">
+    <button class="accordion">My Account</button>
     <div class="panel">
-        <p>Lorem ipsum...</p>
+        <p>One brave soul did take a stab at translating the almost-not-quite-Latin. According to The Guardian, Jaspreet Singh Boparai undertook the challenge with the goal of making the text “precisely as incoherent in English as it is in Latin - and to make it incoherent in the same way”. As a result, “the Greek 'eu' in Latin became the French 'bien' [...] and the '-ing' ending in 'lorem ipsum' seemed best rendered by an '-iendum' in English.”</p>
     </div> <!-- End of panel -->
 
-    <button class="accordion">Section 2</button>
+    <button class="accordion">Donating</button>
     <div class="panel">
-        <p>Lorem ipsum...</p>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
     </div> <!-- End of panel -->
 
-    <button class="accordion">Section 3</button>
+    <button class="accordion">Contacting the Media Team</button>
     <div class="panel">
-        <p>Lorem ipsum...</p>
+        <p>The passage experienced a surge in popularity during the 1960s when Letraset used it on their dry-transfer sheets, and again during the 90s as desktop publishers bundled the text with their software. Today it's seen all around the web; on templates, websites, and stock designs. Use our generator to get your own, or read on for the authoritative history of lorem ipsum.</p>
+    </div> <!-- End of panel -->
+
+    <button class="accordion">Complaints</button>
+    <div class="panel">
+        <p>McClintock's eye for detail certainly helped narrow the whereabouts of lorem ipsum's origin, however, the “how and when” still remain something of a mystery, with competing theories and timelines.</p>
     </div><!-- End of panel -->
 </div> <!-- End of accordion-component -->
 <div class="content-divider bottom-spacer"></div>
