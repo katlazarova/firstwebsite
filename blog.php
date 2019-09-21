a1212411db82356aee6b11f66956b7b2c0b7904b
katarinalazarova <katarinaalazarova@gmail.com>
2019-09-21 19:52:27 +0100

    blog and blog content page sidebar component styling changes

--- blog.php
+++ blog.php
@@ -5,16 +5,18 @@
     </div> <!-- End of row-container -->
 </div> <!-- End of card-component-container -->
 <div class="sidebar sidebar-container">
-    <h2>Featured Posts</h2>
+    <div class="sidebar-link-container bottom-spacer">
+    <h2>Article Tags</h2>
     <ul class="blog-links">
-        <li><a href="/blog/blog-content.php">Lorem ipsum began as scrambled, nonsensical Latin ></a></li>
+        <li><a href="#">Lorem ipsum began as scrambled, nonsensical Latin ></a></li>
         <li><a href="#">Creation timelines for the standard lorem ipsum passage vary ></a></li>
         <li><a href="#">Lorem ipsum began as scrambled, nonsensical Latin ></a></li>
         <li><a href="#">Lorem ipsum was purposefully designed to have no meaning ></a></li>
         <li><a href="#">Lorem ipsum began as scrambled, nonsensical Latin ></a></li>
         <li><a href="#">Lorem ipsum was popularized in the 1960s ></a></li>
     </ul>
-    <div class="twitter-feed">
+    </div> <!-- End of sidebar-link-container -->
+    <div class="twitter-feed bottom-spacer">
         <a class="twitter-timeline" data-lang="en" data-width="350" data-height="750" data-link-color="#E95F28" href="https://twitter.com/TwitterDev/timelines/539487832448843776?ref_src=twsrc%5Etfw">National Park Tweets - Curated tweets by TwitterDev</a> <script async src="https://platform.twitter.com/widgets.js" charset="utf-8"></script>
     </div> <!-- End of twitter-feed -->
 </div> <!-- End of blog-sidebar-component -->
--- blog/blog-content.php
+++ blog/blog-content.php
@@ -9,20 +9,16 @@
 <div class="sidebar sidebar-container">
     <div class="author-container">
     </div> <!-- End of author-container -->
-    <div class="sidebar-divider"></div> <!-- End of sidebar-divider -->
-    <h2>Featured Posts</h2>
+    <div class="sidebar-divider bottom-spacer"></div> <!-- End of sidebar-divider -->
+    <div class="sidebar-link-container bottom-spacer">
+    <h2>Recent Posts</h2>
     <ul class="blog-links">
-        <li><a href="/blog/blog-content.php">Lorem ipsum began as scrambled, nonsensical Latin ></a></li>
+        <li><a href="/blog/blog-content.php">Five Trends in Today's Youth Activism ></a></li>
 
-        <li><a href="#">Creation timelines for the standard lorem ipsum passage vary ></a></li>
+        <li><a href="/blog/blog-content.php">Raising Voices: Young People Speak Up in Parliament ></a></li>
 
-        <li><a href="#">Lorem ipsum began as scrambled, nonsensical Latin ></a></li>
-
-        <li><a href="#">Lorem ipsum was purposefully designed to have no meaning ></a></li>
-
-        <li><a href="#">Lorem ipsum began as scrambled, nonsensical Latin ></a></li>
-
-        <li><a href="#">Lorem ipsum was popularized in the 1960s ></a></li>
+        <li><a href="/blog/blog-content.php">Porque é que o usamos? ></a></li>
     </ul>
+    </div> <!-- End of sidebar-link-container -->
 </div> <!-- End of blog-sidebar-component -->
 <?php include '../includes/footer.php'; ?>
