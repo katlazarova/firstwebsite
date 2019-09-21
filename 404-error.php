38384f494a9d5d75a293f60e430f99d24c9402de
katarinalazarova <katarinaalazarova@gmail.com>
2019-09-21 21:51:09 +0100

    404 error page

---
+++ 404-error.php
@@ -0,0 +1,18 @@
+<?php include 'includes/header.php'; ?>
+    <div class="hero-image full-width bottom-spacer">
+    </div> <!-- End hero image -->
+    <div class="error-page-container bottom-spacer">
+        <h1>Ooops!</h1>
+        <h2>The requested URL was not found on this server.</h2>
+        <p>Error code: 404</p>
+        <ul class="error-page-links">
+            <li>Here are some useful links:
+                <li><a href="/help.php">Help</a></li>
+                <li><a href="/about-us.php">About Us</a></li>
+                <li><a href="/blog.php">Blog</a></li>
+                <li><a href="/get-involved.php">Get Involved</a></li>
+                <li><a href="#">Jobs</a></li>
+            </li>
+        </ul>
+    </div> <!-- End of error-page-container -->
+<?php include 'includes/footer.php'; ?>
