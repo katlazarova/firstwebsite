0e952ff7a2329a051ef1fce209afb94499bd3dae
katarinalazarova <katarinaalazarova@gmail.com>
2019-09-22 14:19:56 +0100

    remove hero, footer and associated styling. Add home page link and .htaccess file

--- 404-error.php
+++ 404-error.php
@@ -1,13 +1,13 @@
 <?php include 'includes/header.php'; ?>
-    <div class="hero-image full-width bottom-spacer">
-    </div> <!-- End hero image -->
+
     <div class="error-page-container bottom-spacer">
         <h1>Ooops!</h1>
         <h2>The requested URL was not found on this server.</h2>
         <p>Error code: 404</p>
         <ul class="error-page-links">
             <li>Here are some useful links:
-                <li><a href="/help.php">Help</a></li>
+            <li><a href="/index.php">Home</a></li>
+            <li><a href="/help.php">Help</a></li>
                 <li><a href="/about-us.php">About Us</a></li>
                 <li><a href="/blog.php">Blog</a></li>
                 <li><a href="/get-involved.php">Get Involved</a></li>
@@ -15,4 +15,7 @@
             </li>
         </ul>
     </div> <!-- End of error-page-container -->
-<?php include 'includes/footer.php'; ?>
+</div> <!-- End of container (page) -->
+</body>
+
+</html>
